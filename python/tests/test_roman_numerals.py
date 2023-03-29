2a52b1dd570ce1ac7ae9330ebbc06ea9de2a34c1
Luz <luz.maria.agc@gmail.com>
2023-03-29 23:50:03 +0200

    test_returns_roman_numerals

--- python/tests/test_roman_numerals.py
+++ python/tests/test_roman_numerals.py
@@ -17,6 +17,12 @@ class TestRomanNumerals:
         (35, "XXXV"),
         (40, "XL"),
         (50, "L"),
+        (90, "XC"),
+        (100, "C"),
+        (400, "CD"),
+        (500, "D"),
+        (900, "CM"),
+        (1000, "M"),
     ]
 
     @pytest.mark.parametrize("given,expected", numbers)
